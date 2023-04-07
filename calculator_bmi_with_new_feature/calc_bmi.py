4dc57e9c6ad4ef00a1138e70524fff0544bad77a
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-08 01:56:15 +0300

    add's input age

--- calculator_bmi_with_new_feature/calc_bmi.py
+++ calculator_bmi_with_new_feature/calc_bmi.py
@@ -5,30 +5,49 @@ value_infinite_loop = True
 while (value_infinite_loop):
   if input("""\n               quit the calculator? 
       ('q' to exit, any input for continue):\n""").lower() == 'q':
-           
     break
 
   else:
     person_gender = input('Enter ur gender (male/female): ').lower()
-    bmi = float(input('Enter weaght(kg): ')) / (float(input('Enter height(cm): '))*0.01)**2
-
+    person_age = (input('Enter ur age: '))
+    bmi = float(input('Enter weaght(kg): ')) /\
+          (float(input('Enter height(cm): '))*0.01)**2 
     chart_bmi =  '========================'
 
 
     if 16 >= round(bmi):
-      print('BMI is too small'+"\n"*2)
+      print('BMI is too small'+"\n")
 
     elif round(bmi) < 40:
-      li = list(chart_bmi)
-      li[round(bmi) - 17] = '|'
+        li = list(chart_bmi)
+        li[round(bmi) - 17] = '|'
 
-      if person_gender == 'male' or 'female':
-        print(f"\nGender = {person_gender}")
-        print(f"BMI = {round(bmi)}")
-        print(f"16{''.join(li)}40"+"\n"*2)
-      else:
-        print(f"\nBMI = {round(bmi)}")
-        print(f"16{''.join(li)}40"+"\n"*2)
+        if person_gender == 'male' or person_gender == 'female':
+          if person_age.isdigit():
+            print(f"\nAge = {person_age}")  
+            print(f"Gender = {person_gender}")
+            print(f"BMI = {round(bmi)}")
+            print(f"16{''.join(li)}40")
+
+            """  """
+          else:
+            print(f"\nGender = {person_gender}")
+            print(f"BMI = {round(bmi)}")
+            print(f"16{''.join(li)}40")
+
+            """  """
+        else:
+          if person_age.isdigit():
+            print(f"\nAge = {person_age}")  
+            print(f"BMI = {round(bmi)}")
+            print(f"16{''.join(li)}40")
+
+            """  """
+          else:
+            print(f"\nBMI = {round(bmi)}")
+            print(f"16{''.join(li)}40")
+
+            """  """
 
     else:
-      print('BMI is too big'+"\n"*3)
+      print('BMI is too big'+"\n")       
