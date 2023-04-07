424aebfb3e2a06aad26970afc262ffffd79b42ad
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-08 01:14:25 +0300

    add's multi-line and output gender person

--- calculator_bmi_with_new_feature/calc_bmi.py
+++ calculator_bmi_with_new_feature/calc_bmi.py
@@ -3,24 +3,32 @@
 value_infinite_loop = True
 
 while (value_infinite_loop):
-  if input('quit the calculator? ()') == 'y':
+  if input("""\n               quit the calculator? 
+      ('q' to exit, any input for continue):\n""").lower() == 'q':
+           
     break
 
   else:
+    person_gender = input('Enter ur gender (male/female): ').lower()
     bmi = float(input('Enter weaght(kg): ')) / (float(input('Enter height(cm): '))*0.01)**2
 
     chart_bmi =  '========================'
 
 
     if 16 >= round(bmi):
-      print('BMI is too small'+"\n"*3)
+      print('BMI is too small'+"\n"*2)
 
     elif round(bmi) < 40:
       li = list(chart_bmi)
       li[round(bmi) - 17] = '|'
 
-      print(f"BMI = {round(bmi)}")
-      print(f"16{''.join(li)}40"+"\n"*3)
+      if person_gender == 'male' or 'female':
+        print(f"\nGender = {person_gender}")
+        print(f"BMI = {round(bmi)}")
+        print(f"16{''.join(li)}40"+"\n"*2)
+      else:
+        print(f"\nBMI = {round(bmi)}")
+        print(f"16{''.join(li)}40"+"\n"*2)
 
     else:
       print('BMI is too big'+"\n"*3)
