75be0081418da844487172188eb7c7b770b185fc
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-09 14:00:02 +0300

    simple code

--- homework_5.py
+++ homework_5.py
@@ -3,30 +3,21 @@ zero = int(0)
 
 print(f"Наши значения: {A}")
 
-# №1
-for i in range (1):
-  while A[i] and A[i+1] and A[i+2]:
-    print("Нет нулевых значений!!!")
-    break
-    
+# №1  
+print(A[0]and A[1]and A[2] and "Нет нулевых значений!!!")
 
 # №2
-print(list(A)[0] or list(A)[1] or list(A)[2] or "Введены все нули!")
+print(A[0] or A[1] or A[2] or "Введены все нули!")
 
 # №3
-if list(A)[0] >  list(A)[1] + list(A)[2]:
-  if list(A)[1] < 0 and list(A)[2] < 0:
-    print(f"a - b - c = {list(A)[0] + list(A)[1] + list(A)[2]}")
+if A[0] >  A[1] + A[2]:
+  if A[1] < 0 and A[2] < 0:
+    print(f"a - b - c = {A[0] + A[1] + A[2]}")
   elif list(A)[2] < 0:
-    print(f"a - b - c = {list(A)[0] - list(A)[1] + list(A)[2]}")
+    print(f"a - b - c = {A[0] - A[1] + A[2]}")
   else:
-    print(f"a - b - c = {list(A)[0] + list(A)[1] - list(A)[2]}")
+    print(f"a - b - c = {A[0] + A[1] - A[2]}")
 
 # №4
-if list(A)[0] <  list(A)[1] + list(A)[2]:
-  if list(A)[1] < 0 and list(A)[2] < 0:
-    print(f"b + c - a = {-list(A)[0] + list(A)[1] + list(A)[2]}")
-  elif list(A)[2] < 0:
-    print(f"b + c - a = {-list(A)[0] + list(A)[1] + list(A)[2]}")
-  else:
-    print(f"b + c - a = {-list(A)[0] + list(A)[1] + list(A)[2]}")  
+if A[0] <  A[1] + A[2]:
+  print(f"b + c - a = {-A[0] + A[1] + A[2]}")  
