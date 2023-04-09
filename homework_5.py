725ff5830bc8539be84040346945fb606647080e
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-09 16:09:51 +0300

    full homework

--- homework_5.py
+++ homework_5.py
@@ -1,6 +1,4 @@
 A = [ int(input(f"Напишите {i+1} значение: ")) for i in range(3) ]
-zero = int(0)
-
 print(f"Наши значения: {A}")
 
 # №1  
@@ -11,13 +9,18 @@ print(A[0] or A[1] or A[2] or "Введены все нули!")
 
 # №3
 if A[0] >  A[1] + A[2]:
-  if A[1] < 0 and A[2] < 0:
-    print(f"a - b - c = {A[0] + A[1] + A[2]}")
-  elif list(A)[2] < 0:
-    print(f"a - b - c = {A[0] - A[1] + A[2]}")
-  else:
-    print(f"a - b - c = {A[0] + A[1] - A[2]}")
-
+  print(f"({A[0]}) - ({A[1]}) - ({A[2]}) = \
+        {A[0] - A[1] - A[2]}")
+ 
 # №4
 if A[0] <  A[1] + A[2]:
-  print(f"b + c - a = {-A[0] + A[1] + A[2]}")  
+  print(f"({A[1]}) + ({A[2]}) - ({A[0]}) = \
+        {-A[0] + A[1] + A[2]}")  
+
+# №5
+if A[0] > 50 and ((A[1] > A[0]) or (A[2] > A[0])):
+  print('Вася')
+
+# №6
+if A[0] > 5 or ((A[1] == 7) and (A[2] == 7)):
+  print('Петя')
