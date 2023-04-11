491ddf385c18f6f70175bfb5da0ef9146a7a1b56
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-11 04:14:24 +0300

    add feature with choice menu

--- calculator_bmi_with_menu/main.py
+++ calculator_bmi_with_menu/main.py
@@ -1,4 +1,5 @@
-import os
+import os, pprint
+
 menu = {
      1:'Вывести список пользователей',
      2:'Посмотреть информацию о пользователе(рост, вес, ИМТ)',
@@ -7,37 +8,37 @@ menu = {
      5:'Добавить пользователя',
      6:'Выход',
  }
-dict_person = {}
+
 def print_menu():
      for key in menu.keys():
-         print(f"{key}. {menu[key]}")
+        print(f"{key}. {menu[key]}")
 
-def choice_1():
-      os.system('CLS')
-      print('Вывести список пользователей')      
+def show_history(state):      
+      return state      
 
 def choice_2():
-      os.system('CLS')
       print('Посмотреть информацию о пользователе(рост, вес, ИМТ)')
 
 def choice_3():
-      os.system('CLS')
       print('Изменить данные о пользователе')      
 
 def choice_4():
-      os.system('CLS')
       print('Удалить выбранного пользователя')      
 
-def choice_5():
-      os.system('CLS')
-      print('Добавить пользователя')      
+def calculator_bmi(weight, height):
+      return round(float(weight)/((float(height)*0.01)**2))      
 
 os.system('CLS')
+_state={}
+id=0
 while(True):
+  _weight,_height,_gender,_age=70,190,"male",24
   print_menu()
   choice = int(input(f"\n Ваш выбор: "))
   if choice == 1:
-     choice_1()
+      os.system('CLS')
+      pprint.pprint(show_history(_state))
+      print()
   elif choice == 2:
       choice_2()
   elif choice == 3:
@@ -45,7 +46,15 @@ while(True):
   elif choice == 4:
       choice_4()
   elif choice == 5:
-      choice_5()
+    _state[id]={
+        'weight':_weight,
+        'height':_height,
+        'gender':_gender,
+        'age':_age,
+        'bmi':(calculator_bmi(_weight,_height))
+        }
+    id+=1
+    os.system('CLS')
   elif choice == 6:
       break
   else:
