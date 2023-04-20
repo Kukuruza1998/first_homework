f4e7affb48d13b2038bd2444e4c13c50ec725699
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-20 16:33:45 +0300

    fix bug adds more readable code

--- card_game_21/main.py
+++ card_game_21/main.py
@@ -1,50 +1,73 @@
 import os, random
 
 from card import card_list
-
+deck = card_list.card_list
 
 def clear_Screen():
     os.system("CLS")
 
 def check_card_in_deck():
-    if len(card_list.card_list)-1 == 0:
+    if not deck:
       print('Карты в колоде закончились',end='\n'*2)
       exit()
-    return card_list.card_list.pop(random.randint(1, len(card_list.card_list)-1))
+    
+    return check_on_ace()
 
-def check_to_more(count_el_card):
-    if count_el_card <= 21:
-      ask_add_card = input('Взять карту(y/n)? ')
-      if ask_add_card == "y":
-        card_adds = check_card_in_deck()
-        count_el_card += card_adds
+def check_on_ace():
+    card_input = deck.pop(random.randint(0, len(deck)-1))
+    if card_input == 1:
+      print(f"Пришёл туз, добавим 11 или 1?")
+      ace = input()
+      if ace == '1':
+          card_input = int(ace)
+      elif ace == '11':
+          card_input = int(ace)
+    
+    return card_input
+           
 
-        print(f"Пришла: {card_adds}. Итого: {count_el_card}")
-        check_to_more(count_el_card)
-      elif ask_add_card == "n":
-        clear_Screen()
-        print(f"Ваш результат: {count_el_card}",end='\n'*2)
-        return exit
-      else:
-        clear_Screen()
-        print('Научись тыкать на буквы',end='\n'*2)
+def print_a_card(count_el_card):
+    card_adds = check_card_in_deck()
+    count_el_card += card_adds
+    print(f"Пришла: {card_adds}. Итого: {count_el_card}")
+    check_on_21(count_el_card)
+
+def check_on_21(count_el_cards):
+    if count_el_cards < 21:
+      check_to_add_more_card(count_el_cards)
+    elif count_el_cards == 21:
+      print('Победа, 21 очко',end='\n'*2)
+      exit
     else:
-        clear_Screen()
-        print(f"Перебор, итог: {count_el_card}",end='\n'*2)
-        return exit
+      clear_Screen()
+      print(f"Перебор, итог: {count_el_cards}",end='\n'*2)
+      return exit
+
+def check_to_add_more_card(count_el_card):
+    ask_add_card = input('Взять карту(y/n)? ')
+    if ask_add_card == "y":
+      print_a_card(count_el_card)
+      
+    elif ask_add_card == "n":
+      clear_Screen()
+      print(f"Ваш результат: {count_el_card}",end='\n'*2)
+      return exit
+    else:
+      clear_Screen()
+      print('Научись тыкать на буквы',end='\n'*2)
 
 def main():
     clear_Screen()
     while True:
-      input_card = check_card_in_deck()
+      first_card = check_card_in_deck()
       second_card = check_card_in_deck() 
-      print(f"Первой пришла карта номиналом: {input_card}, второй: {second_card}")
-      if (input_card == 1 or input_card == 11) and (second_card == 1 or second_card == 11):
+      print(f"Первой пришла карта номиналом: {first_card}, вторая: {second_card}")
+      if first_card == 1 and second_card == 1:
         print("Победа, два туза")
         exit 
       else:
-        print(f"Итого: {input_card + second_card}")
-        check_to_more(input_card + second_card)
+        print(f"Итого: {first_card + second_card}")
+        check_on_21(first_card + second_card)
 
 
 if __name__ == "__main__":
