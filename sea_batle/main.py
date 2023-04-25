9aa9776fb5c93079d946713fa38d2a3c064e13d1
Kukuruza Alexandr <mrsilly998@mail.ru>
2023-04-25 17:15:31 +0300

    edit code add more func

--- sea_batle/main.py
+++ sea_batle/main.py
@@ -5,7 +5,7 @@ def clear_screen():
     return os.system('CLS')
 
 def convert_letter_to_str(y):
-        CONVERTER = {
+    CONVERTER = {
             'A': 0,
             'B': 1,
             'C': 2,
@@ -17,7 +17,7 @@ def convert_letter_to_str(y):
             'I': 8,
             'J': 9
         }
-        return CONVERTER[y]
+    return CONVERTER[y]
 
 
 class SeaBattle:
@@ -54,72 +54,89 @@ class SeaBattle:
 
     def check_near_ship(self, x, j):
         if self.is_ship(x+1, j) or \
-            self.is_ship(x+1, j+1) or \
-            self.is_ship(x+1, j-1) or \
-            self.is_ship(x, j+1) or \
-            self.is_ship(x-1, j) or \
-            self.is_ship(x-1, j-1) or \
-            self.is_ship(x-1, j+1) or \
-            self.is_ship(x, j-1):
-          return True
-        
+              self.is_ship(x+1, j+1) or \
+              self.is_ship(x+1, j-1) or \
+              self.is_ship(x, j+1) or \
+              self.is_ship(x-1, j) or \
+              self.is_ship(x-1, j-1) or \
+              self.is_ship(x-1, j+1) or \
+              self.is_ship(x, j-1):
+            return True
+            
+    def validation_place_ship_vertical(self, ships_sizes, size, y, x):
+        if x + size > self.size:
+            clear_screen()
+            print("Корабль выходит за границы поля. Попробуйте еще раз.")
+        for i in range(x, x+size):
+            if self.is_ship(i, y):
+                clear_screen()
+                print("Корабль пересекается с другим кораблем. Попробуйте еще раз.")
+                break
+            elif self.check_near_ship(x, y) == True:
+                clear_screen()
+                print("Корабль в пределах другого корабля. Попробуйте еще раз.")
+                break
+        else:
+            return True
+
+    def validation_place_ship_horizontal(self, ships_sizes, size, y, x):
+        if y + size > self.size:
+            clear_screen()
+            print("Корабль выходит за границы поля. Попробуйте еще раз.")
+        for j in range(y, y+size):
+            if self.is_ship(x, j):
+                clear_screen()
+                print("Корабль пересекается с другим кораблем. Попробуйте еще раз.")
+                break
+            elif self.check_near_ship(x, j) == True:
+                clear_screen()
+                print("Корабль в пределах другого корабля. Попробуйте еще раз.")
+                break
+        else:
+            return True
+
     def setup_board(self):
         ships_sizes = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
         clear_screen()
         for size in ships_sizes:
-          while True:
-              print(self)
-              print(f"Корабль размером {size}")
-              y = convert_letter_to_str(
-                    input('Введите столбец(A-J): ').upper()
-                  )
-              x = int(input('Введите строку(1-10): '))
-              orientation = input("Введите ориентацию (vertical / horizontal): ")
-              if orientation == 'v':
-                    if x + size > self.size:
-                        clear_screen()
-                        print("Корабль выходит за границы поля. Попробуйте еще раз.")
-                        continue
-                    for i in range(x, x+size):
-                        if self.is_ship(i, y):
-                            clear_screen()
-                            print("Корабль пересекается с другим кораблем. Попробуйте еще раз.")
-                            break
-                        elif self.check_near_ship(x, y) == True:
-                            clear_screen()
-                            print("Корабль в пределах другого корабля. Попробуйте еще раз.")
-                            break
-                    else:
+            while True:
+                print(self)
+                print(f"Корабль размером {size}")
+                y = convert_letter_to_str(
+                      input('Введите столбец(A-J): ').upper()
+                    )
+                x = int(input('Введите строку(0-9): '))
+                orientation = input("Введите ориентацию (vertical / horizontal): ")
+                if orientation == 'v':
+                    if self.validation_place_ship_vertical(self, size, y, x) == True:
                         for i in range(x, x+size):
                             clear_screen()
                             self.place_ship(i, y)
-                        self.ships.append((x, y, size, orientation))
+                            self.ships.append((x, y, size, orientation))
                         break
-                
-              elif orientation == 'h':
-                    if y + size > self.size:
-                        clear_screen()
-                        print("Корабль выходит за границы поля. Попробуйте еще раз.")
-                        continue
-                    for j in range(y, y+size):
-                        if self.is_ship(x, j):
-                            clear_screen()
-                            print("Корабль пересекается с другим кораблем. Попробуйте еще раз.")
-                            break
-                        elif self.check_near_ship(x, j) == True:
-                            clear_screen()
-                            print("Корабль в пределах другого корабля. Попробуйте еще раз.")
-                            break
-                    else:
+                elif orientation == 'h':
+                    if self.validation_place_ship_horizontal(self, size, y, x) == True:
                         for j in range(y, y+size):
                             clear_screen()
                             self.place_ship(x, j)
-                        self.ships.append((x, y, size, orientation))
+                            self.ships.append((x, y, size, orientation))
                         break
+                else:
+                    clear_screen()
+                    print("Выбрано неверное направление")
+                  
 
 
 
 game = SeaBattle(10)
 game.setup_board()
-print(game.is_hit(2, 3)) # проверяем на хит
-print(game.is_miss(5, 5)) # на промах
+game2 = SeaBattle(10)
+game2.setup_board()
+# print(game.is_hit(2, 3)) 
+# print(game.is_miss(5, 5)) 
+
+print(game)
+print(game2)
+
+#убираем корабли с поля, но знаем их данные. Делаем бесконечный цикл на то чтобы выводить поля 1 и 2 добавляя к ним импут
+#если импут == корабль, то выводим хит, если нет то мисс. если хит==тру, то стреляем дальше. while true(пока кто-то не выбьет все корабли)
